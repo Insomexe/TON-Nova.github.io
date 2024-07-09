7f5835ddb592b1225284e76b07a6fc2c1b0d589e
Insomnia <164337341+Insomexe@users.noreply.github.com>
2024-07-09 22:15:36 +0300

    Update main.tsx

--- coin-nova/src/main.tsx
+++ coin-nova/src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'https://insomexe.github.io/Nova-Token/tonconnect-manifest.json';
+const manifestUrl = 'https://insomexe.github.io/TON-Nova.github.io/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
